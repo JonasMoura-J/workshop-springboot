4e3f277ea34b15f439b55a2e5dde6e1b4a5c6bf5
JonasMoura-J <jonasmourat9090@gmail.com>
2026-03-30 18:09:20 -0300

    associação one to many para product OrderItem

--- src/main/java/com/jonas/course/entities/Product.java
+++ src/main/java/com/jonas/course/entities/Product.java
@@ -1,6 +1,8 @@
 package com.jonas.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import org.aspectj.weaver.ast.Or;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -26,6 +28,9 @@ public class Product implements Serializable {
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"),
     inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product(){
 
@@ -81,6 +86,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> orders = new HashSet<>();
+        for (OrderItem o : items){
+            orders.add(o.getOrder());
+        }
+        return orders;
     }
 
     @Override
