fa64e8da3053787ec9543cfaa0ee1d9c3ed2f654
JonasMoura-J <jonasmourat9090@gmail.com>
2026-03-30 18:18:17 -0300

    associação one to one para pagamento

--- src/main/java/com/jonas/course/entities/Order.java
+++ src/main/java/com/jonas/course/entities/Order.java
@@ -35,6 +35,9 @@ public class Order implements Serializable {
 
     private Integer orderStatus;
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order(){
 
     }
@@ -82,6 +85,14 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems(){
         return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
